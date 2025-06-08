82bb59dde9ac19906c7640ff43bfdb3f7c761179
Jirkatix <jirkatix@seznam.cz>
2025-06-08 16:52:25 +0200

    Added update over UART

---
+++ Source/BNO055_Firmware_Update_Utility (UART)/BNO055_Firmware_Update_Utility/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace BNO055_Firmware_Update_Utility
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(67, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Upload BNO055 firmware";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(142, 271);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(245, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 4;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(67, 96);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(229, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(250, 243);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "0 %";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Select port:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = global::BNO055_Firmware_Update_Utility.Properties.Resources.schematicuart_drawio;
+            this.pictureBox1.Location = new System.Drawing.Point(154, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(58, 31);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(64, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Wiring diagram:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(302, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(183, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Only ports 9 and lower are supported.";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 35);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(25, 24);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "1.";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(12, 138);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(25, 24);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "3.";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(12, 80);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(25, 24);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "2.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 476);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ Source/BNO055_Firmware_Update_Utility (UART)/BNO055_Firmware_Update_Utility/Form1.cs
@@ -0,0 +1,278 @@
+﻿using BNO055_Firmware_Update_Utility.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+using System.Management;
+
+namespace BNO055_Firmware_Update_Utility
+{
+    public partial class Form1 : Form
+    {
+        string dir_path = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            string assembly_loc = System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase;
+            string loc = assembly_loc.Substring(8, assembly_loc.LastIndexOf('/') - 8);
+            string[] files = System.IO.Directory.GetFiles(loc, "*.bin");
+            dir_path = loc;
+            if (files.Length > 1)
+            {
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                MessageBox.Show("Found more than one firmware file.", "Error", buttons);
+                this.Close();
+            }
+
+            if (files.Length == 0)
+            {
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                MessageBox.Show("No firmware file found.", "Error", buttons);
+                this.Close();
+            }
+
+            _hexData = File.ReadAllBytes(files[0]);
+
+            if (_hexData.Length != 0x3c000)
+            {
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                MessageBox.Show("File is crap.", "Error", buttons);
+                this.Close();
+            }
+
+            string fn = files[0].Substring(files[0].LastIndexOf('\\') + 1);
+
+            this.Text = "BNO055 Firmware Update Utility (UART version) - Ready to upload file: " + fn;
+
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE Caption like '%(COM%'"))
+            {
+                var portnames = SerialPort.GetPortNames();
+                var ports = searcher.Get().Cast<ManagementBaseObject>().ToList().Select(p => p["Caption"].ToString());
+
+                var portList = portnames.Select(n => ports.FirstOrDefault(s => s.Contains(n))).ToList();
+
+                foreach (string s in portList)
+                {
+                    comboBox1.Items.Add(s);
+                    if (comboBox1.Items.Count != 0)
+                    {
+                        comboBox1.SelectedIndex = 0;
+                        selected_port = comboBox1.SelectedItem.ToString();
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string comport = "";
+
+            int index = selected_port.IndexOf("COM");
+            if (index != -1)
+            {
+                comport = selected_port.Substring(index, 4);
+            }
+
+            try
+            {
+                serialPort1.PortName = comport;
+                serialPort1.Open();
+            }
+            catch
+            {
+                if (!serialPort1.IsOpen)
+                {
+                    MessageBox.Show("Device not ready.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            backgroundWorker1.RunWorkerAsync();
+            
+        }
+
+        //decompiled stuff from BNO055.dll
+
+        private byte[] _hexData;
+        uint lengthOfHexData_default = 152992;
+
+        //CRC-16/IBM-3740
+        private ushort crc_16bit(byte[] data_p, int length)
+        {
+            int num1 = 0;
+            ushort num2 = ushort.MaxValue;
+            while (length-- > 0)
+            {
+                ushort num3 = (ushort)((uint)(ushort)((uint)data_p[num1++] & (uint)byte.MaxValue) << 8);
+                for (int index = 0; index < 8; ++index)
+                {
+                    if ((((int)num2 ^ (int)num3) & 32768) != 0)
+                        num2 = (ushort)((int)num2 << 1 ^ 4129);
+                    else
+                        num2 <<= 1;
+                    num3 <<= 1;
+                }
+            }
+            return (ushort)~num2;
+        }
+
+        private void UpdateCRC(byte[] data, int len)
+        {
+            ushort num = this.crc_16bit(data, len);
+            data[len++] = (byte)((uint)~num >> 8);
+            data[len] = (byte)~num;
+        }
+
+        string selected_port;
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            byte[] data1 = new byte[6];
+            byte[] data2 = new byte[3] { (byte)30, (byte)0, (byte)0 };
+            data1[0] = (byte)(lengthOfHexData_default >> 24 & (uint)byte.MaxValue);
+            data1[1] = (byte)(lengthOfHexData_default >> 16 & (uint)byte.MaxValue);
+            data1[2] = (byte)(lengthOfHexData_default >> 8 & (uint)byte.MaxValue);
+            data1[3] = (byte)(lengthOfHexData_default & (uint)byte.MaxValue);
+
+
+            double num1 = Math.Ceiling((double)lengthOfHexData_default / (double)data2[0]);
+
+
+            this.UpdateCRC(data1, 4);
+
+            if (this.SendCommands(data1))
+            {
+                this.UpdateCRC(data2, 1);
+                if (this.SendCommands(data2))
+                {
+                    uint lengthOfHexData = lengthOfHexData_default;
+                    int percentProgress = 1;
+                    do
+                    {
+                        if (lengthOfHexData < (uint)data2[0])
+                            data2[0] = (byte)lengthOfHexData;
+                        byte[] numArray = new byte[(int)data2[0] + 2];
+                        Array.Copy((Array)this._hexData, (long)(lengthOfHexData_default - lengthOfHexData), (Array)numArray, 0L, (long)data2[0]);
+                        this.UpdateCRC(numArray, (int)data2[0]);
+                        if (!this.SendCommands(numArray))
+                        {
+                            error = true;
+                            e.Cancel = true;
+                            return;
+                        }
+
+                        lengthOfHexData -= (uint)data2[0];
+                        if ((long)((uint)(((int)lengthOfHexData_default - (int)lengthOfHexData) * 100) / lengthOfHexData_default) - (long)percentProgress >= 0L)
+                        {
+                            ++percentProgress;
+                            backgroundWorker1.ReportProgress(percentProgress);
+                        }
+                    }
+                    while (lengthOfHexData > 0U);
+                }
+                else
+                {
+                    error = true;
+                    e.Cancel = true;
+                }
+            }
+            else
+            {
+                error = true;
+                e.Cancel = true;
+            }
+            
+        }
+
+        private void TransmitToProtocol(byte[] data)
+        {
+            serialPort1.Write(data, 0, data.Length);
+
+            bool bno_responded = false;
+            while (!bno_responded)
+            {
+                if (serialPort1.BytesToRead == 1)
+                {
+                    bno_responded = true;
+                }
+            }
+
+            receivedAcknowledgement = (byte)serialPort1.ReadByte();
+        }
+
+
+        bool error = false;
+        byte receivedAcknowledgement = 0;
+
+        private bool SendCommands(byte[] data)
+        {
+            bool flag = false;
+
+            this.TransmitToProtocol(data);
+
+            if (receivedAcknowledgement != (byte)115)
+            {
+                error = true;
+                backgroundWorker1.CancelAsync();
+                MessageBox.Show("CRC failed during transmission", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (receivedAcknowledgement == (byte)115)
+                flag = true;
+            receivedAcknowledgement = (byte)0;
+            return flag;
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.ProgressPercentage <= 100)
+            {
+                progressBar1.Value = e.ProgressPercentage;
+                label1.Text = e.ProgressPercentage.ToString() + " %";
+            } else
+            {
+                progressBar1.Value = 100;
+                label1.Text = "100 %";
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if(!error)
+            {
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                MessageBox.Show("Update finished", "Done", buttons);
+            } else
+            {
+                MessageBox.Show("Update failed", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selected_port = comboBox1.SelectedItem.ToString();
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.BringToFront();
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+    }
+}
